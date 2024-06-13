bd872712e3ba439e93c97e0c3f42b27d7f372b57
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-13 23:55:06 +0200

    refactor: append lines with using `normal`

--- lua/chainsaw/read-write-lines.lua
+++ lua/chainsaw/read-write-lines.lua
@@ -8,7 +8,8 @@ local function warn(msg) vim.notify(msg, vim.log.levels.WARN, { title = "Chainsa
 ---@param logLines string|string[]
 ---@param varsToInsert string[]
 function M.appendLines(logLines, varsToInsert)
-	local ln = vim.api.nvim_win_get_cursor(0)[1]
+	local ln, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local indent = vim.api.nvim_get_current_line():match("^%s*")
 	if type(logLines) == "string" then logLines = { logLines } end
 
 	-- Prevent nested quotes making logs invalid.
@@ -23,23 +24,16 @@ function M.appendLines(logLines, varsToInsert)
 		logLines = vim.tbl_map(function(line) return line:gsub(quotesInVar, repl) end, logLines)
 	end
 
-	-- ensure correct indentation
-	local indentBasedFts = { "python", "yaml", "elm" }
-	local isIndentBased = vim.tbl_contains(indentBasedFts, vim.bo.ft)
-	local indent = isIndentBased and vim.api.nvim_get_current_line():match("^%s*") or ""
-	local action = isIndentBased and "j" or "j=="
-
-	-- insert all the lines
+	-- insert all lines
 	for _, line in pairs(logLines) do
 		local toInsert = indent .. line:format(unpack(varsToInsert))
 		vim.api.nvim_buf_set_lines(0, ln, ln, true, { toInsert })
-		normal(action)
 		ln = ln + 1
 	end
-end
 
----@param msg string
-local function warnNotify(msg) vim.notify(msg, vim.log.levels.WARN, { title = "Chainsaw" }) end
+	-- move cursor down to last inserted line
+	vim.api.nvim_win_set_cursor(0, { ln, col })
+end
 
 ---@param logType string
 ---@param logsData logStatementData
