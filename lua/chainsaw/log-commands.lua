7edcedda09e47f56b33f767a0fb3f369aadb44ca
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-21 17:01:55 +0200

    fix(removeLogs): movement of cursor to position of deleted logs

--- lua/chainsaw/log-commands.lua
+++ lua/chainsaw/log-commands.lua
@@ -98,7 +98,9 @@ function M.removeLogs()
 
 	-- escape for vim regex, in case `[]()` are used in the marker
 	local toRemove = config.marker:gsub("([%[%]()])", "\\%1")
+	local cursorPos = vim.api.nvim_win_get_cursor(0)
 	vim.cmd(("silent global/%s/delete"):format(toRemove))
+	vim.api.nvim_win_set_cursor(0, cursorPos)
 	vim.cmd.nohlsearch()
 
 	-- notify on number of lines removed
