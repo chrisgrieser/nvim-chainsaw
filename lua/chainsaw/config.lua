263e82716547c4161ed57a2c16375aa29ee8c9e3
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-08-11 12:16:52 +0200

    docs: clarify usage of `marker` config

--- lua/chainsaw/config.lua
+++ lua/chainsaw/config.lua
@@ -3,18 +3,19 @@ local M = {}
 
 ---@class pluginConfig
 local defaultConfig = {
-	-- The marker should be a unique string, since `removeLogs` will remove
-	-- any line with it. Emojis or strings like "[Chainsaw]" are recommended.
+	-- The marker should be a unique string, since lines with it are highlighted
+	-- and since `removeLogs` will remove any line with it. Thus, emojis or
+	-- strings like "[Chainsaw]" are recommended.
 	marker = "🪚",
-
-	-- emojis used for `beepLog`
-	beepEmojis = { "🔵", "🟩", "⭐", "⭕", "💜", "🔲" },
 
 	-- Highlight lines with the marker.
 	-- lazy.nvim users:, you need to add `event = VeryLazy` to the plugin spec to
 	-- have existing log statements highlighted as well.
 	---@type string|false false to disable
 	logHighlightGroup = "Visual",
+
+	-- emojis used for `beepLog`
+	beepEmojis = { "🔵", "🟩", "⭐", "⭕", "💜", "🔲" },
 
 	logStatements = require("chainsaw.log-statements-data"),
 }
