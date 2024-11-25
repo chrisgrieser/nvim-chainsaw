e83e19b23a8d35e146f0114dd6a5bd320a042999
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-26 00:00:31 +0100

    fix: detection of icon being in `marker`

--- lua/chainsaw/utils.lua
+++ lua/chainsaw/utils.lua
@@ -4,14 +4,15 @@ local M = {}
 ---@param msg string
 function M.warn(msg)
 	local marker = require("chainsaw.config").config.marker
-	local icon = vim.api.nvim_strwidth(marker) < 2 and marker or nil
+	-- `3` to account for emojis/nerdfont that have a width of `2`
+	local icon = vim.api.nvim_strwidth(marker) < 3 and marker or nil
 	vim.notify(msg, vim.log.levels.WARN, { title = "chainsaw", icon = icon })
 end
 
 ---@param msg string
 function M.info(msg)
 	local marker = require("chainsaw.config").config.marker
-	local icon = vim.api.nvim_strwidth(marker) < 2 and marker or nil
+	local icon = vim.api.nvim_strwidth(marker) < 3 and marker or nil
 	vim.notify(msg, vim.log.levels.INFO, { title = "chainsaw", icon = icon })
 end
 
