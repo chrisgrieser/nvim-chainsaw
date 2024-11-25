0b139918d78381fa2202f6e48d0fc5f686a5a86c
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-25 18:47:59 +0100

    improv: if marker is icon, warnings use it as icon (`snacks.nvim` only)

--- lua/chainsaw/utils.lua
+++ lua/chainsaw/utils.lua
@@ -2,8 +2,13 @@ local M = {}
 --------------------------------------------------------------------------------
 
 ---@param msg string
-function M.warn(msg) vim.notify(msg, vim.log.levels.WARN, { title = "chainsaw" }) end
+function M.warn(msg)
+	local marker = require("chainsaw.config").config.marker
+	local icon = vim.api.nvim_strwidth(marker) < 2 and marker or nil
+	vim.notify(msg, vim.log.levels.WARN, { title = "chainsaw", icon = icon })
+end
 
+---@param msg string
 function M.info(msg)
 	local marker = require("chainsaw.config").config.marker
 	local icon = vim.api.nvim_strwidth(marker) < 2 and marker or nil
