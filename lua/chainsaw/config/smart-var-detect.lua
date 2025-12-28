db85b0b6a7b7584d031ae11ac064be8df05f15a5
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-12-28 11:34:30 +0100

    chore: shorter variable names

--- lua/chainsaw/config/smart-var-detect.lua
+++ lua/chainsaw/config/smart-var-detect.lua
@@ -8,16 +8,16 @@ M.ftConfig = {
 	lua = function(node)
 		if not node:parent() then return node end
 
-		local cursorOnField = node:parent():type() == "dot_index_expression"
+		local onField = node:parent():type() == "dot_index_expression"
 			and (node:prev_named_sibling() ~= nil) -- not for the first field
-		local cursorOnArrayNumber = node:parent():type() == "bracket_index_expression"
-		if cursorOnField or cursorOnArrayNumber then return node:parent() end
+		local onArrayNumber = node:parent():type() == "bracket_index_expression"
+		if onField or onArrayNumber then return node:parent() end
 
-		local cursorOnMethod = node:parent():type() == "method_index_expression"
+		local onMethod = node:parent():type() == "method_index_expression"
 			and (node:prev_named_sibling() ~= nil) -- not for the first field
-		if cursorOnMethod then return node:parent():parent() end
-		local cursorOnColonOfMethod = node:type() == "method_index_expression"
-		if cursorOnColonOfMethod then return node:parent() end
+		if onMethod then return node:parent():parent() end
+		local onColonOfMethod = node:type() == "method_index_expression"
+		if onColonOfMethod then return node:parent() end
 		local cursorParensOfMethod = node:type() == "arguments"
 			and (node:prev_named_sibling():type() == "method_index_expression")
 		if cursorParensOfMethod then return node:prev_named_sibling():parent() end
@@ -25,22 +25,22 @@ M.ftConfig = {
 		return node
 	end,
 	python = function(node)
-		local cursorOnField = node:type():find("^string_")
+		local onField = node:type():find("^string_")
 			and node:parent()
 			and node:parent():parent()
 			and node:parent():parent():type() == "subscript"
-		if cursorOnField then node = node:parent():parent() end
+		if onField then node = node:parent():parent() end
 
-		local cursorOnAttribute = node
+		local onAttribute = node
 			and node:type() == "identifier"
 			and node:parent()
 			and node:parent():type() == "attribute"
-		if cursorOnAttribute and node then node = node:parent() end
+		if onAttribute and node then node = node:parent() end
 		return node
 	end,
 	javascript = function(node)
-		local cursorOnField = node:type() == "property_identifier"
-		if cursorOnField then node = node:parent() end
+		local onField = node:type() == "property_identifier"
+		if onField then node = node:parent() end
 		return node
 	end,
 }
