dde0f8a4e92e64536cdee1fe26184a8f503e9e5f
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-12-28 11:33:30 +0100

    feat(lua): smart variable detection now supports methods

--- lua/chainsaw/config/smart-var-detect.lua
+++ lua/chainsaw/config/smart-var-detect.lua
@@ -7,10 +7,21 @@ local M = {}
 M.ftConfig = {
 	lua = function(node)
 		if not node:parent() then return node end
+
 		local cursorOnField = node:parent():type() == "dot_index_expression"
-			and (node:prev_named_sibling() ~= nil)
+			and (node:prev_named_sibling() ~= nil) -- not for the first field
 		local cursorOnArrayNumber = node:parent():type() == "bracket_index_expression"
-		if cursorOnField or cursorOnArrayNumber then node = node:parent() end
+		if cursorOnField or cursorOnArrayNumber then return node:parent() end
+
+		local cursorOnMethod = node:parent():type() == "method_index_expression"
+			and (node:prev_named_sibling() ~= nil) -- not for the first field
+		if cursorOnMethod then return node:parent():parent() end
+		local cursorOnColonOfMethod = node:type() == "method_index_expression"
+		if cursorOnColonOfMethod then return node:parent() end
+		local cursorParensOfMethod = node:type() == "arguments"
+			and (node:prev_named_sibling():type() == "method_index_expression")
+		if cursorParensOfMethod then return node:prev_named_sibling():parent() end
+
 		return node
 	end,
 	python = function(node)
