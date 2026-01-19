d254d94cdd3a604d2eebd2ddc136dbecf2023390
Mike Boiko <mike@boiko.ca>
2026-01-18 20:57:46 -0700

    stylua formatting

--- lua/chainsaw/config/smart-insert-location.lua
+++ lua/chainsaw/config/smart-insert-location.lua
@@ -11,7 +11,9 @@ M.ftConfig = {
 		-- return statement
 		local parent = node:parent()
 		while parent and not parent:type():find("function") do
-			if parent:type() == "return_statement" then return -1 end
+			if parent:type() == "return_statement" then
+				return -1
+			end
 			parent = parent:parent()
 		end
 
@@ -24,12 +26,16 @@ M.ftConfig = {
 	end,
 	javascript = function(node)
 		local parent = node:parent()
-		if not parent then return end
+		if not parent then
+			return
+		end
 
 		-- return statement
 		local inReturnStatement = parent:type() == "return_statement"
 			or (parent:parent() and parent:parent():type() == "return_statement")
-		if inReturnStatement then return -1 end
+		if inReturnStatement then
+			return -1
+		end
 
 		-- multiline assignment
 		local isAssignment = parent:type() == "variable_declarator"
@@ -52,16 +58,24 @@ M.ftConfig = {
 		local parent = node:parent()
 		while parent do
 			local type = parent:type()
-			if type == "function_definition" or type == "async_function_definition" then break end
+			if type == "function_definition" or type == "async_function_definition" then
+				break
+			end
 			parent = parent:parent()
 		end
-		if not parent then return end
+		if not parent then
+			return
+		end
 
 		local body = parent:field("body")[1]
-		if not body then return end
+		if not body then
+			return
+		end
 
 		local firstChild = body:named_child(0)
-		if not firstChild then return end
+		if not firstChild then
+			return
+		end
 
 		-- check for docstrings
 		if firstChild:type() == "expression_statement" then
@@ -69,7 +83,9 @@ M.ftConfig = {
 			if expression and expression:type() == "string" then
 				local _, _, endRow, _ = firstChild:range()
 				local startRow, _, _, _ = node:range()
-				if startRow > endRow then return end
+				if startRow > endRow then
+					return
+				end
 				return endRow - startRow
 			end
 		end
